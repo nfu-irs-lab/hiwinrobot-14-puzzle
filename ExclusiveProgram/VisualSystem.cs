6471d2217853b1eac345755efe28e41faf067cc5
VincentYeh-dev <vincent3314534>
2022-05-21 22:12:42 +0800

    增加VisualSystem

--- ExclusiveProgram/VisualSystem.cs
+++ ExclusiveProgram/VisualSystem.cs
@@ -1,103 +1,158 @@
-﻿using Emgu.CV;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Emgu.CV.Structure;
+﻿using System.Diagnostics;
+using Emgu.CV;
+using Emgu.CV.Util;
 using Emgu.CV.CvEnum;
-using System.Drawing;
+using Emgu.CV.Structure;
+using Emgu.CV.Features2D;
+using Emgu.CV.Flann;
 
-namespace Puzzle.Framework
+namespace ExclusiveProgram
 {
-    public class RawPuzzle
+    public class VisualSystem
     {
-        public Point Position{ set; get; }
-        public double Angle_degree{ set; get; }
-        public Size Size { set; get; }
-
-    }
-    public enum PuzzleShape
-    {
-
-    }
-
-    public enum PuzzleContent
-    {
-
-    }
-
-    public class PuzzlePiece
-    {
-        public PuzzleType type {  get; }
-        public Image<Bgr,Byte> Image {  get; }
-
-        public double Angle_degree{ set; get; }
-        public Size Size { set; get; }
-
-        public PuzzlePiece(PuzzleType type,double angle,Size size,Image<Bgr,Byte> Image)
+        private VisualSystem()
         {
-            this.type = type;
-            this.Image = Image;
-            this.Angle_degree = angle;
-            this.Size = size;
+
         }
-    }
 
-    public enum PuzzleType
-    {
-        
-    }
-    public interface IPuzzleLocator
-    {
-        List<RawPuzzle> Locate(Image<Bgr, Byte> input);
-    }
+        private static VideoCapture camera=new VideoCapture();
 
-    public interface IPuzzleCorrector
-    {
-        Image<Bgr,Byte> Correct(Image<Bgr, Byte> input,RawPuzzle raw);
-    }
-
-    public interface IPuzzleRecognizer
-    {
-        List<PuzzleType> Recognize(List<Image<Bgr, Byte>> input);
-    }
-
-    public interface IPuzzleFactory
-    {
-        List<PuzzlePiece> Execute(Image<Bgr, Byte> input);
-    }
-
-    public class DefaultPuzzleFactory : IPuzzleFactory
-    {
-        private IPuzzleRecognizer recognizer;
-        private IPuzzleLocator locator;
-        private IPuzzleCorrector corrector;
-
-        public DefaultPuzzleFactory(IPuzzleRecognizer recognizer,IPuzzleLocator locator,IPuzzleCorrector corrector)
+        public static void setCameraResolution(int resolution_width,int resolution_height)
         {
-            this.recognizer = recognizer;
-            this.locator = locator;
-            this.corrector = corrector;
+            camera.Set(CapProp.FrameWidth,resolution_width);
+            camera.Set(CapProp.FrameHeight,resolution_height);
         }
-        public List<PuzzlePiece> Execute(Image<Bgr, byte> input)
+
+        public static Image<Bgr,byte> CaptureImage()
         {
-            List<RawPuzzle> raws=locator.Locate(input);
-            List<Image<Bgr,Byte>> PuzzleImages=new List<Image<Bgr, byte>>();
-            foreach(RawPuzzle piece in raws)
+            Mat image = camera.QueryFrame();
+            return image.Clone().ToImage<Bgr, byte>();
+        }
+
+        #region 圖片預處理
+
+        #region 圖片二值化
+
+        public static Image<Gray,byte> Binarization(Image<Gray,byte> image,int threshold)
+        {
+            Image<Gray,byte> dst=new Image<Gray,byte>(image.Width,image.Height);
+            CvInvoke.Threshold(image, dst, threshold, 255, ThresholdType.Binary);
+            return dst;
+        }
+
+        #endregion 圖片二值化
+
+
+        public static Image<Gray,byte> BgrToGray(Image<Bgr,byte> image)
+        {   
+            Image<Gray,byte> dst=new Image<Gray,byte>(image.Width,image.Height);
+            CvInvoke.CvtColor(image, dst, ColorConversion.Bgr2Gray);
+            return dst;
+        }
+
+
+        #region 圖片顏色拓展
+
+        public static Image<Bgr, byte> ExtendColor(Image<Bgr, byte> image)
+        {
+            Image<Bgr, byte> img = new Image<Bgr, byte>(image.Size);
+
+            int rowsCounts = image.Rows;
+            int colsCounts = image.Cols;
+
+            int min = 256, max = 0;
+
+            for (int i = 0; i < rowsCounts; i++)
             {
-                PuzzleImages.Add(corrector.Correct(input, piece));
+                for (int j = 0; j < colsCounts; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        byte buffer;
+                        buffer = image.Data[i, j, k];
+                        if ((int)buffer < min)
+                        { min = (int)buffer; }
+
+                        if (buffer > max)
+                        { max = buffer; }
+                    }
+                }
             }
 
-            List<PuzzleType> types=recognizer.Recognize(PuzzleImages);
-               
-            List<PuzzlePiece> puzzles=new List<PuzzlePiece>();
-            for(int i = 0; i < PuzzleImages.Count; i++)
+            for (int i = 0; i < rowsCounts; i++)
             {
-                puzzles.Add(new PuzzlePiece(types[i],raws[i].Angle_degree,raws[i].Size,PuzzleImages[i]));
+                for (int j = 0; j < colsCounts; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        double d = image.Data[i, j, k];
+                        d = d - min;
+                        d = d / max;
+                        d = d * 255;
+                        img.Data[i, j, k] = (byte)d;
+                    }
+                }
             }
-            return puzzles;
+
+            return img;
         }
+
+        #endregion 圖片顏色拓展
+
+        #region 白平衡
+
+        /// <summary>
+        /// 圖像白平衡
+        /// </summary>
+        /// <param name="img"></param>
+        /// <returns></returns>
+        public static Image<Bgr, byte> WhiteBalance(Image<Bgr, byte> img)
+        {
+            int avgR = 0, avgG = 0, avgB = 0;
+            int sumR = 0, sumG = 0, sumB = 0;
+            for (int h = 0; h < img.Height; ++h)
+            {
+                for (int w = 0; w < img.Width; ++w)
+                {
+                    sumB += img.Data[h, w, 0];
+                    sumG += img.Data[h, w, 1];
+                    sumR += img.Data[h, w, 2];
+                }
+            }
+            int size = img.Height * img.Width;
+            avgB = sumB / size;
+            avgG = sumG / size;
+            avgR = sumR / size;
+            double k = 0.299 * avgR + 0.587 * avgG + 0.114 * avgB;
+
+            avgR = (sumR / size);
+            avgG = (sumG / size);
+            avgB = (sumB / size);
+
+            double kr = k / avgR;
+            double kg = k / avgG;
+            double kb = k / avgB;
+            double newB, newG, newR;
+            for (int h = 0; h < img.Height; ++h)
+            {
+                for (int w = 0; w < img.Width; ++w)
+                {
+                    newB = img.Data[h, w, 0] * kb;
+                    newG = img.Data[h, w, 1] * kg;
+                    newR = img.Data[h, w, 2] * kr;
+
+                    img.Data[h, w, 0] = (byte)(newB > 255 ? 255 : newB);
+                    img.Data[h, w, 1] = (byte)(newG > 255 ? 255 : newG);
+                    img.Data[h, w, 2] = (byte)(newR > 255 ? 255 : newR);
+                }
+            }
+
+            return img;
+        }
+
+        #endregion 白平衡
+
+        #endregion 圖片預處理
     }
 
 }
